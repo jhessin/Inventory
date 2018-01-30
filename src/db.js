055b913a668455701731c5d5bf05e2f8b9b05757
Jim Hessin <jhessin@gmail.com>
2018-01-30 16:50:11 -0700

    Getting flow figured out.

--- src/db.js
+++ src/db.js
@@ -5,66 +5,65 @@ import _ from 'lodash';
 
 export default firebase;
 
-type StringArgs = {
-	clearFilter: boolean,
-	filterBy: Object,
-	onUpdate: Function,
-	path: string,
-	sortBy: string,
+type Args = {
+	clearFilter?: boolean,
+	filterBy?: Object,
+	onUpdate?: Function,
+	path?: string,
+	sortBy?: string,
 };
-
-type PathArgs = StringArgs & {
-	path: Path,
-};
-
-export type Args = StringArgs | PathArgs;
 
 export class Path {
 	// Private Data
+
+	// The ref to the Path
 	_ref = firebase.database().
 		ref();
 
-	// The ref to the Path
-	_path = '';
-
 	// The string of the Path
-	_sortBy = '';
+	_path: string = '';
 
 	// A field to sort the data by when getting the dataArray
-	_filterBy = {};
+	_sortBy: string = '';
 
 	// Key value pairs to filter the list by.
-	_data = {};
+	_filterBy: Object = {};
 
 	// The data stored by this Path
+	_data: Object = {};
+
+	// The callback to call when the data is updated
 	_onUpdate = () => {}; // A callback to be run when the data is updated.
 
-	constructor({
-		path = '',
-		sortBy = '',
-		filterBy = {},
-		clearFilter = false,
-		onUpdate = () => {},
-	}: Args) {
-		// Set up all the data to populate the _data content.
-		if(path instanceof Path) {
-			this._path = path._path || '';
-			this._sortBy = sortBy || path._sortBy || '';
-			this._filterBy = clearFilter
-				? filterBy || {}
-				: {
-					...path._filterBy,
-					...filterBy,
-				};
-			this._ref = path._ref;
-			this._onUpdate = onUpdate || path._onUpdate || (() => {});
-		}else {
-			this._path = path;
-			this._onUpdate = onUpdate || (() => {});
-			this._ref = firebase.database().
-				ref(path);
-			this._sortBy = sortBy || '';
-			this._filterBy = filterBy || {};
+	// eslint-disable-next-line
+	constructor(path: Path | string | null = '', args: ?Args) {
+		if(args) {
+			const {
+				sortBy = '',
+				filterBy = {},
+				clearFilter = false,
+				onUpdate = () => {},
+			} = args;
+			// Set up all the data to populate the _data content.
+			if(path instanceof Path) {
+				this._path = path._path || '';
+				this._sortBy = sortBy || path._sortBy || '';
+				this._filterBy = clearFilter
+					? filterBy || {}
+					: {
+						...path._filterBy,
+						...filterBy,
+					};
+				this._ref = path._ref;
+				this._onUpdate = onUpdate || path._onUpdate || (() => {});
+			}else {
+				this._path = args.path || '';
+				this._onUpdate = onUpdate || (() => {});
+				this._ref = firebase.database().
+					ref(path);
+				this._sortBy = sortBy || '';
+				this._filterBy = filterBy || {};
+			}
 		}
 
 		// A standard system to update the _data when there is a change.
@@ -74,10 +73,10 @@ export class Path {
 
 			const value = snap.val();
 			// Filter the list by the filterBy criteria.
-			if(typeof value === 'object') for(let filter in filterBy) if(
-				filterBy.hasOwnProperty(filter) &&
+			if(typeof value === 'object') for(let filter in this._filterBy) if(
+				this._filterBy.hasOwnProperty(filter) &&
 						value.hasOwnProperty(filter) &&
-						value[filter] !== filterBy[filter]
+						value[filter] !== this._filterBy[filter]
 			) return;
 
 			// Save the value to the _data object.
@@ -99,42 +98,50 @@ export class Path {
 			this._onUpdate();
 		};
 
-		this._ref.on('child_added', updateData);
+		this._ref.on(
+			'child_added', updateData
+		);
 
-		this._ref.on('child_removed', snap => {
-			if(delete this._data[snap.key]) this._onUpdate();
-		});
+		this._ref.on(
+			'child_removed', snap => {
+				if(delete this._data[snap.key]) this._onUpdate();
+			}
+		);
 
-		this._ref.on('child_changed', updateData);
+		this._ref.on(
+			'child_changed', updateData
+		);
 	}
 
 	onUpdate(onUpdate: Function) {
-		if(typeof onUpdate === 'function') return new Path({
-			path: this,
-			onUpdate,
-		});
+		if(typeof onUpdate === 'function') return new Path(
+			this, {onUpdate}
+		);
 	}
 
 	get path(): string {
 		return this._path;
 	}
 
-	get data() {
+	get data(): ?mixed {
 		return this._data;
 	}
 
 	set data(obj: ?mixed) {
-		this.ref.update(obj);
+		this._ref.update(obj);
 	}
 
-	get dataArray() {
-		if(!this._data || typeof this._data !== 'object') return [];
+	get dataArray(): Array<Object> {
+		if(!this._data || typeof this._data !== 'object') return [
+		];
 
 		// Shortcut for faster loading
 		// eslint-disable-next-line no-magic-numbers
-		if(Object.keys(this._data).length === 0) return [];
+		if(Object.keys(this._data).length === 0) return [
+		];
 
-		let arr = [];
+		let arr = [
+		];
 
 		Object.keys(this._data).
 			forEach(key => {
@@ -142,38 +149,44 @@ export class Path {
 			});
 
 		// Sort the data by it's sortBy string
-		arr = _.sortBy(arr, key => key[this._sortBy] || key.key);
+		arr = _.sortBy(
+			arr, key => {
+				if(key && typeof key === 'object') return key[this._sortBy] || key.key;
+			}
+		);
 
 		return arr;
 	}
 
-	child = childPath => {
-		const path = {
-			...this,
-			_path: this._path + childPath,
-		};
-		return new Path({path});
+	child = (childPath: string) => {
+		return new Path(
+			this, {path: this._path + childPath}
+		);
 	};
 
 	push = data => this._ref.push({
 		...data,
-		...this.filterBy,
+		...this._filterBy,
 	});
 
 	remove = () => this._ref.remove();
 
-	filter = (filterBy = {}, clearFilter = true) => new Path({
-		path: this,
-		filterBy,
-		clearFilter,
-	});
+	filter = (
+		filterBy = {}, clearFilter = true
+	) => new Path(
+		this, {
+			filterBy,
+			clearFilter,
+		}
+	);
 
-	addFilter = (filterBy = {}) => this.filter(filterBy, false);
+	addFilter = (filterBy = {}) => this.filter(
+		filterBy, false
+	);
 
-	sort = (sortBy = '') => new Path({
-		path: this,
-		sortBy,
-	});
+	sort = (sortBy = '') => new Path(
+		this, {sortBy}
+	);
 }
 
 // Create an all time User
@@ -182,29 +195,33 @@ firebase.auth().
 		user.data = currentUser;
 		if(currentUser) {
 			user.uid = currentUser.uid;
-			user.path = ({
-				path, ...args
-			}) => new Path({
-				path: `${user.uid}/${path}`,
-				...args,
-			});
+			user.path = (
+				path, {...args}
+			) => new Path(
+				(`${user.uid}/${path}`: string), {...args}
+			);
 		}else {
-			user.uid = null;
+			user.uid = '';
 			user.path = () => null;
 		}
-		_.forEach(user.listeners, value => value.callback());
+		_.forEach(
+			user.listeners, value => value.callback()
+		);
 	});
 
 export const user = {
-	create: (...args) => firebase.auth().
+	create: (...args: [string]) => firebase.auth().
 		createUserWithEmailAndPassword(...args),
-	signIn: (...args) => firebase.auth().
+	signIn: (...args: [string]) => firebase.auth().
 		signInWithEmailAndPassword(...args),
 	signOut: () => firebase.auth().
 		signOut(),
-	listeners: [], // An array of callbacks
-	subscribe: (obj, meth = 'onUpdate') => {
-		let callback;
+	listeners: [
+	], // An array of callbacks
+	subscribe: (
+		obj: mixed, meth?: string = 'onUpdate'
+	) => {
+		let callback: Function | null;
 		const id = obj;
 		if(typeof meth === 'function' && obj) {
 			callback = meth;
@@ -226,9 +243,18 @@ export const user = {
 		}
 	},
 	unsubscribe: obj => {
-		_.remove(user.listeners, listener => listener.id === obj);
+		_.remove(
+			user.listeners, listener => listener.id === obj
+		);
 	},
 	get exists() {
 		return user.uid && user.path;
 	},
+	data: {},
+	uid : '',
+	path: (
+		path: string, args: Args
+	) => new Path(
+		path, args
+	),
 };
