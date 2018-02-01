89c9eae34bb8d9af8495122c02de2b4a4d7db65c
Jim Hessin <jhessin@gmail.com>
2018-01-31 17:56:47 -0700

    Fixed some issues with the new eslint config.

--- src/data/fieldHelpers.js
+++ src/data/fieldHelpers.js
@@ -1,84 +1,85 @@
 // @flow
 
-import { user } from '../db';
+import { user, Path } from '../db';
 
 export type FieldType = {
-  typeName: string,
-  validate: mixed => boolean,
+	typeName: string,
+	validate: mixed => boolean,
 };
 
 export type Args = {
-  named: ?string,
-  tableId: ?string,
-  type: ?FieldType,
-  value: ?mixed,
+	named: ?string,
+	tableId: ?string,
+	type: ?FieldType,
+	value: ?mixed,
 };
 
 // An array of FieldTypes
 export const fieldTypes: Array<FieldType> = [
-  {
-    typeName: 'string',
-    validate: value => typeof value === 'string',
-  },
-  {
-    typeName: 'number',
-    validate: value => typeof value === 'number',
-  },
-  {
-    typeName: 'boolean',
-    validate: value => typeof value === 'boolean',
-  },
-  {
-    typeName: 'date',
-    validate: value => value instanceof Date,
-  },
+	{
+		typeName: 'string',
+		validate: value => typeof value === 'string',
+	},
+	{
+		typeName: 'number',
+		validate: value => typeof value === 'number',
+	},
+	{
+		typeName: 'boolean',
+		validate: value => typeof value === 'boolean',
+	},
+	{
+		typeName: 'date',
+		validate: value => value instanceof Date,
+	},
 ];
 
 export const verify = ({ type, value }: Args) => {
-  if (
-    type &&
-    typeof type === 'object' &&
-    type.hasOwnProperty('validate') &&
-    typeof type.validate === 'function'
-  ) {
-    return type.validate(value);
-  }
+	if (
+		type &&
+		typeof type === 'object' &&
+		type.hasOwnProperty('validate') &&
+		typeof type.validate === 'function'
+	) {
+		return type.validate(value);
+	}
 
-  return false;
+	return false;
 };
 
 export const pushField = (data: ?Object) => {
-  if (data) {
-    if (data.id) {
-      let path = user.path(`Fields/${data.id}`);
-      if (path) return (path.data = data);
-    } else return user.path('Fields').push(data);
-  }
+	if (data) {
+		if (data.id) {
+			let path = user.path(`Fields/${data.id}`);
+			if (path) return (path.data = data);
+		} else return user.path('Fields').push(data);
+	}
+	return new Path('Fields').push(data);
 };
 
 export const createField = ({ named, type, tableId }: Args) => {
-  if (!named || !user) return null;
+	if (!named || !user) return null;
 
-  switch (type) {
-    case 'string':
-    case 'number':
-    case 'boolean':
-    case 'date':
-      return {
-        id: pushField().key,
-        fieldName: named,
-        fieldType: type,
-      };
-    case 'link':
-      if (!tableId) return null;
+	switch (type) {
+		case 'string':
+		case 'number':
+		case 'boolean':
+		case 'date':
+			return {
+				id: pushField().key,
+				fieldName: named,
+				fieldType: type,
+			};
+		case 'link':
+			if (!tableId) return null;
 
-      return {
-        id: pushField().key,
-        fieldName: named,
-        // fieldType: type,
-      };
+			return {
+				id: pushField().key,
+				fieldName: named,
+				// fieldType: type,
+			};
 
-    default:
-      return null;
-  }
+		default:
+			return null;
+	}
 };
